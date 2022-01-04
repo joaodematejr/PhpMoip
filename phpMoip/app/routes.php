7ac40231b2d5df53945ea01a3dc319ef44f6804f
João Dematé <joao.demate@alquimidiacorp.com.br>
2022-01-04 16:55:04 -0300

    att

--- phpMoip/app/routes.php
+++ phpMoip/app/routes.php
@@ -19,9 +19,5 @@ return function (App $app) {
         return $response;
     });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
 
 };
