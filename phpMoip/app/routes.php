4f0d9114579d012b67f321ab98c8831605e50c85
joaodematejr <joaodematejr@gmail.com>
2022-01-04 17:02:55 -0300

    att

--- phpMoip/app/routes.php
+++ phpMoip/app/routes.php
@@ -19,16 +19,5 @@ return function (App $app) {
         return $response;
     });
 
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
-
-    
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
-
 
 };
